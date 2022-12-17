fc8b4dc6b9854535627c9f282319b1e63c55b768
phiarchitect <phi@phiarchitect.com>
2022-12-17 06:46:15 -0800

    cleanup structure

--- README.rst
+++ README.rst
@@ -1,1 +1,7 @@
 The **PHOTON platform - ablog** is forked from https://github.com/sunpy/ablog
+
+ABlog is a Sphinx extension for generating a log oriented static site. It
+can be easily integrated with documentation for Python modules.
+
+
+
--- ablog/conf.py
+++ ablog/conf.py
@@ -186,7 +186,7 @@ exclude_patterns = ['.docs', 'tests']
 # show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'gruvbox-dark'
 
 # A list of ignored prefixes for module index sorting.
 # modindex_common_prefix = []
