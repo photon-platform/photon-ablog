296510fff52cf0f5be07d349cd7355ad7ce6e73b
phiarchitect <phi@phiarchitect.com>
2023-01-08 19:13:07 -0800

    update

--- ablog/conf.py
+++ ablog/conf.py
@@ -173,7 +173,7 @@ language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['.docs', 'tests']
+exclude_patterns = ['.archive', '.docs', 'tests']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
