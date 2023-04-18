bdd745567e5afa24b88486053419cb1921b1d934
phiarchitect <phi@phiarchitect.com>
2023-04-17 21:48:37 -0700

    exclude egg

--- ablog/conf.py
+++ ablog/conf.py
@@ -177,7 +177,7 @@ language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['.archive', '.docs', 'tests']
+exclude_patterns = ['.archive', '.docs', 'tests', '*.egg-info']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
